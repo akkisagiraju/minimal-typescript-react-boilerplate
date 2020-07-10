71ff428e84c5630f7011e462e0cf33b7df540ddf
Akhil <7601150+akkisagiraju@users.noreply.github.com>
2020-07-10 14:35:21 +0530

    grammar - 'an' to 'a'

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Hello, this is an React app written in TypeScript!</h1>
+      <h1>Hello, this is a React app written in TypeScript!</h1>
     </div>
   );
 };
